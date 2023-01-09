1c9ae99ab5398fbec7ff39e0e47e8f5918d28b8f
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-01-09 15:19:03 +0000

    HTML login form

--- src/templates/Login/index.tsx
+++ src/templates/Login/index.tsx
@@ -1,10 +1,23 @@
-import './styles.css'
+import './styles.css';
 
 import { Component } from 'react'
 
 export class Login extends Component {
   render() {
-    return <h1>Login</h1>
+    return (
+      <div className="Container-Login">
+        <form action="../Random-Users" className="Form-Login">
+          <img src="" alt="Logo" />
+          <input type="text" className="Input-Username" minLength={6} placeholder="Nome de usuário" required />
+          <input type="password" className="Input-Password" minLength={6} placeholder="Senha" required />
+          <p>
+            <img src="" alt="Checkbox" />
+            Lembrar de mim
+          </p>
+          <input type="submit" value="Entrar" />
+        </form>
+      </div>
+    );
   }
 }
 
