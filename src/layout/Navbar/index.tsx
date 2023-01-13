b3413c6f5b8e8d2767b0fef446766df9b88a58d1
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-01-13 18:28:20 -0300

    style navbar

--- src/layout/Navbar/index.tsx
+++ src/layout/Navbar/index.tsx
@@ -1,28 +1,31 @@
+import './styles.css'
+
 import { Link } from 'react-router-dom'
 import { Component } from 'react'
+import Logo from '../../images/Logo.png'
 
 export class Navbar extends Component {
   render() {
     return (
       <>
         <nav className="navbar">
-          <div className="titulo">Logo</div>
+          <img src={Logo} alt="" className='Navbar-Logo'/>
           <div className="links">
             <ul>
               <li>
-                <Link to="/Random-Users">Random Users</Link>
+                <Link to="/Random-Users" className='Nav-link'>Random Users</Link>
               </li>
               <li>
-                <Link to="/HTTP-Cats">HTTP Cats</Link>
+                <Link to="/HTTP-Cats" className='Nav-link'>HTTP Cats</Link>
               </li>
               <li>
-                <Link to="/Random-Dogs">Random Dogs</Link>
+                <Link to="/Random-Dogs" className='Nav-link'>Random Dogs</Link>
               </li>
               <li>
-                <Link to="/Clients">Clientes</Link>
+                <Link to="/Clients" className='Nav-link'>Clientes</Link>
               </li>
               <li>
-                <Link to="/">Sair</Link>
+                <Link to="/" className='Nav-link'>Sair</Link>
               </li>
             </ul>
           </div>
