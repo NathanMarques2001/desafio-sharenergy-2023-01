2cf879cc4aaea86d51be2ebcadd4e248e4ac912d
Nathan Marques de Aguiar Brandão <108840944+NathanMarques2001@users.noreply.github.com>
2023-01-13 02:13:58 -0300

    Random user

--- src/templates/Random-Users/index.tsx
+++ src/templates/Random-Users/index.tsx
@@ -2,54 +2,122 @@ import './styles.css'
 
 import Navbar from '../../layout/Navbar'
 
-import { Component } from 'react'
+import { useEffect, useState } from 'react'
 import UserCard from '../../components/User-Card'
 
-export class RandomUsers extends Component {
-  state = {
-    name: '',
-    username: '',
-    age: 0,
-    email: '',
-    photo: '',
+export interface RandomUser {
+  name: {
+    first: string
+    last: string
   }
 
-  componentDidMount(): void {
-    this.getUsers()
+  login: {
+    username: string
   }
 
-  getUsers = async () => {
-    fetch('https://randomuser.me/api/')
-      .then((response) => response.json())
-      .then((data) =>
-        this.setState({
-          name: `${data.results[0]['name']['first']} ${data.results[0]['name']['last']}`,
-          username: data.results[0]['login']['username'],
-          age: data.results[0]['dob']['age'],
-          email: data.results[0]['email'],
-          photo: data.results[0]['picture']['large'],
-        }),
+  dob: {
+    age: number
+  }
+
+  email: string
+
+  picture: {
+    large: string
+  }
+}
+
+export const RandomUsers = () => {
+  const [user, setUser] = useState<RandomUser[]>([])
+  const [page, setPage] = useState<number>(1)
+  const [searchValue, setSearchValue] = useState<string>('')
+  const maxUsers = 36
+  const maxUsersPerPage = 6
+  let endPage = maxUsers / maxUsersPerPage
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const response = await fetch(
+        `https://randomuser.me/api/?page=${page}&results=${maxUsersPerPage}&seed=abc`,
       )
+      const data = await response.json()
+      setUser(data.results)
+    }
+    getUsers()
+  }, [page])
+
+
+  function nextPage(): void {
+    if (page < endPage) {
+      setPage(page + 1)
+    }
   }
 
-  render() {
-    const { name, username, age, email, photo } = this.state
-    return (
-      <>
-        <Navbar />
-        <div className="RandomUser-Content">
-          <input type="text" placeholder="Insira o nome ou usuário" className='Input-RandomUser'/>
-          <UserCard
-            photo={photo}
-            username={username}
-            name={name}
-            age={age}
-            email={email}
-          />
-        </div>
-      </>
-    )
+  function previousPage(): void {
+    if (page > 1) {
+      setPage(page - 1)
+    }
   }
+
+  function firstPage(): void {
+    setPage(1)
+  }
+
+  function lastPage(): void {
+    setPage(endPage)
+  }
+
+  const lowerValue = searchValue.toLowerCase()
+
+  const filterUser = user.filter(
+    (FillUser) =>
+      FillUser.name.first
+        .toLowerCase()
+        .concat(' ', FillUser.name.last.toLowerCase())
+        .includes(lowerValue) ||
+      FillUser.login.username.toLowerCase().includes(lowerValue) ||
+      FillUser.email.toLowerCase().includes(lowerValue),
+  )
+
+  return (
+    <>
+      <Navbar />
+      <div className="RandomUser-Content">
+        <input
+          type="text"
+          placeholder="Insira o nome ou usuário"
+          className="Input-RandomUser"
+          onChange={(e) => setSearchValue(e.target.value)}
+          value={searchValue}
+        />
+        <div className="Container-Cards">
+          {filterUser.map((users) => (
+            <UserCard
+              photo={users.picture.large}
+              username={users.login.username}
+              name={`${users.name.first} ${users.name.last}`}
+              age={users.dob.age}
+              email={users.email}
+            />
+          ))}
+        </div>
+        <div className="Buttons-div">
+          <button className="Pagination-Button" onClick={(e) => firstPage()}>
+            &#60;&#60;
+          </button>
+          <button className="Pagination-Button" onClick={(e) => previousPage()}>
+            &#60;
+          </button>
+          <div className="AtualPage-div">{page}</div>
+          <button className="Pagination-Button" onClick={(e) => nextPage()}>
+            &#62;
+          </button>
+          <button className="Pagination-Button" onClick={(e) => lastPage()}>
+            &#62;&#62;
+          </button>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default RandomUsers
